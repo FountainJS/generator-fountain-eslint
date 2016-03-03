407efeadb01375507ff4fa98a6a67059cf7310d6
Micaël <micael.mbagira@icloud.com>
2016-03-03 15:34:02 +0100

    Reload browser after compiling

--- generators/app/templates/gulp_tasks/scripts-full.js
+++ generators/app/templates/gulp_tasks/scripts-full.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const browserSync = require('browser-sync');
 
 const conf = require('../conf/gulp.conf');
 
@@ -8,5 +9,6 @@ gulp.task('scripts', scripts);
 function scripts() {
   return gulp.src(conf.path.src('**/*.js'))
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.format())
+    .pipe(browserSync.stream());
 }
