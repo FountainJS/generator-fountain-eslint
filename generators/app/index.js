969dd0ccf7ac547fb699fa0fdaf2f78812944556
Micaël <micael.mbagira@icloud.com>
2016-03-09 17:24:45 +0100

    Add babel dependencies for systemjs

--- generators/app/index.js
+++ generators/app/index.js
@@ -77,6 +77,30 @@ module.exports = fountain.Base.extend({
             }
           }
         });
+      if (this.props.modules === 'systemjs') {
+        if (this.props.framework === 'angular2') {
+          _.merge(pkg, {
+            jspm: {
+              devDependencies: {
+                'babel-plugin-angular2-annotations': 'npm:babel-plugin-angular2-annotations@^5.0.0',
+                'babel-plugin-transform-decorators-legacy': 'npm:babel-plugin-transform-decorators-legacy@^1.3.4',
+                'babel-plugin-transform-class-properties': 'npm:babel-plugin-transform-class-properties@^6.6.0',
+                'babel-plugin-transform-flow-strip-types': 'npm:babel-plugin-transform-flow-strip-types@^6.6.4'
+              }
+            }
+          });
+        } else if (this.props.framework === 'react') {
+          _.merge(pkg, {jspm: {devDependencies: {'babel-preset-react': 'npm:babel-preset-react@6.5.0'}}});
+        }
+
+        if (this.props.js !== 'typescript') {
+          _.merge(pkg, {
+            jspm: {
+              dependencies: {'babel': 'npm:babel-core@^6.6.5'},
+              devDependencies: {'plugin-babel': 'npm:systemjs-plugin-babel@^0.0.8'}
+            }
+          });
+        }
       }
 
       this.mergeJson('package.json', pkg);
