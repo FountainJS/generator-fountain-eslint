edabbdd56588cbd7e189855f56f9fce12f1e7f41
Micaël <micael.mbagira@icloud.com>
2016-03-09 17:37:02 +0100

    Add babel dependencies from gulpfile

--- generators/app/index.js
+++ generators/app/index.js
@@ -100,6 +100,14 @@ module.exports = fountain.Base.extend({
           _.merge(pkg, {jspm: {devDependencies: {'babel-preset-react': 'npm:babel-preset-react@6.5.0'}}});
         }
 
+        if (this.props.js === 'babel' || this.props.js === 'js' && this.props.framework === 'react') {
+          _.merge(pkg, {devDependencies: {'babel-core': '^6.2.0'}});
+        }
+
+        if (this.props.js === 'babel') {
+          _.merge(pkg, {devDependencies: {'babel-preset-es2015': '^6.2.0'}});
+        }
+
         if (this.props.js !== 'typescript') {
           _.merge(pkg, {
             jspm: {
