63f2b27b402ddc843bd9ad79ebf8711732ad4841
Micaël <micael.mbagira@icloud.com>
2016-08-20 14:35:16 +0200

    Disable angular/no-service-method

--- test/utils.js
+++ test/utils.js
@@ -37,6 +37,11 @@ module.exports.angular1Base = {
   devDependencies: {
     'eslint-config-angular': '^0.5.0',
     'eslint-plugin-angular': '^1.3.0'
+  },
+  eslintConfig: {
+    rules: {
+      'angular/no-service-method': 0
+    }
   }
 };
 
